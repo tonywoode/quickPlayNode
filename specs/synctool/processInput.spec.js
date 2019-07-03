63c6115f25cce4cb59ce71d55775216c0212153e
tony woode <tonywoodecode@gmail.com>
2019-07-03 08:47:07 +0100

    [synctool] scaffold input processing for testing

---
+++ specs/synctool/processInput.spec.js
@@ -0,0 +1,6 @@
+const processInput = romPath => {
+
+
+}
+
+module.exports = { processInput }
--- src/synctool/index.js
+++ src/synctool/index.js
@@ -1,3 +1,13 @@
+const processInput = require('./processInput.js')
+const taggedSum = require("daggy").taggedSum
+const SyncState = taggedSum("SyncState", {
+  Move: ["bool"],
+  NoMove: ["bool"],
+  Resume: ["kb"],
+  Error: ["errObj"],
+})
+
+
 const synctool = romPath => {
   console.log(romPath)
   process.exit() //else we'll fall through to the mametool stuff
