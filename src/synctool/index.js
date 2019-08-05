108cfef540a3d0899076a75057ec2e82caddeb0a
tony woode <tonywoodecode@gmail.com>
2019-08-05 22:55:56 +0100

    [synctool] reworked flow from pseudocode

--- src/synctool/index.js
+++ src/synctool/index.js
@@ -5,11 +5,11 @@ const {
   doRootPathsExist,
   checkFile
 } = require('./stateHandlers.js')
-const path = require("path")
-const {getSubDir} = require('./processInput.js')
+const path = require('path')
+const { getSubDir } = require('./processInput.js')
 const configFileName = 'synctool_config.json'
 const objPrint = obj => JSON.stringify(obj, null, 2)
-const { getSize } = require('./checkFiles.js')
+const { getSize, isFile } = require('./checkFiles.js')
 
 const synctool = romPath => {
   checkRomPath(romPath) // check you passed me an input path
@@ -17,26 +17,27 @@ const synctool = romPath => {
     // so we have a valid path and a root path, is path in root path
     .map(config => isRomPathInRootPath(config, romPath))
     // ok relativePath is stated to live under localRoot, but localRoot and remoteRoot need to exist
-    .chain(config => 
+    .chain(config =>
       doRootPathsExist(config)
-        .chain(_ => checkFile(romPath)) // check its not a dir
-        .map(localStat => getSubDir(romPath)(config.localRoot)  
-          // first work out the relative path we'd have on remote
-          //   we know the path.join is safe because we've shown both constituents are safe
-          //   chaining over the result returns us the remotePath, uncontainerised, but we're still in the outer Task
-          .chain(relativePath => { const remotePath = path.join(config.remoteRoot, relativePath)
-            return ({ localStat, remotePath}) }) 
+        .map(_ =>
+          getSubDir(romPath)(config.localRoot)
+            // first work out the relative path we'd have on remote
+            //   we know the path.join is safe because we've shown both constituents are safe
+            //   chaining over the result returns us the remotePath, uncontainerised, but we're still in the outer Task
+            .chain(relativePath => {
+              const remotePath = path.join(config.remoteRoot, relativePath)
+              return remotePath
+            })
         )
-      //so we've got a remotePath, and we know localPath exists, turn the Task into a check of remotePath
-      .chain( ({ localStat, remotePath }) => checkFile(remotePath)
-        .map( remoteStat => {console.log(`local size is ${getSize(localStat)}, remote size is ${getSize(remoteStat)}`
-        ) 
-        return remoteStat
-      }))
-      
-      )
-
-    
+        // so we've got a remotePath, and we know localPath exists, turn the Task into a check of remotePath
+        .chain(remotePath => checkFile(remotePath))
+        .chain(remoteStat =>
+          checkFile(romPath).map(localStat => {
+            console.log(getSize(remoteStat))
+            console.log(getSize(localStat))
+          })
+        )
+    )
 
   checkFiles.run().listen({
     onRejected: rej => console.log(`[synctool] unexpected error: ${rej}`),
