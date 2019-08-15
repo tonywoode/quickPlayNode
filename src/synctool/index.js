08288c1fc82a9a0d36f0ddd4dd8a23aa33bbc9e0
tony woode <tonywoodecode@gmail.com>
2019-08-15 22:45:05 +0100

    [synctool] lose unnecessary nesting

--- src/synctool/index.js
+++ src/synctool/index.js
@@ -11,44 +11,41 @@ const {
   copyIfLocalNotFound
 } = require('./stateHandlers.js')
 const { getSize } = require('./checkFiles.js')
-
 const equal = (a, b) => a === b
+
 // give synctool only the LOCAL path, and a config file that tells it how to make the remotePath
-const synctool = (localPath, configFileName) => {
-  return (
-    checkLocalPath(localPath) // check you passed me an input path
-      .chain(_ => loadConfig(configFileName))
-      // so we have a valid path and a root path, is path in root path
-      .chain(config => isLocalPathInRootPath(config, localPath))
-      // ok relativePath is stated to live under localRoot, but localRoot and remoteRoot need to exist
-      .chain(config =>
-        doRootPathsExist(config)
-          // work out the relative path we'd have on remote
-          .map(_ => calculateRemotePath(localPath, config))
-          .chain(remotePath =>
-            // is file in remote? If not, be specific about why we're exiting
-            checkFile(remotePath)
-              .orElse(fileError => rejected(`File Not In Remote Folder: ${fileError}`))
-              .chain(remoteStat =>
-                checkFile(localPath)
-                  // the files in both places, check dest is smaller, or maybe they are same file
-                  .chain(localStat =>
-                    getSize(remoteStat).chain(remoteSize =>
-                      getSize(localStat).chain(
-                        localSize =>
-                          equal(remoteSize, localSize) // filesize is equal, but check really same before deciding
-                            ? checkReallyEqual(remotePath, localPath)
-                            : copyIfLocalSmaller(localPath, localSize, remotePath, remoteSize)
-                      )
+const synctool = (localPath, configFileName) =>
+  checkLocalPath(localPath) // check you passed me an input path
+    .chain(_ => loadConfig(configFileName))
+    // so we have a valid path and a root path, is path in root path
+    .chain(config => isLocalPathInRootPath(config, localPath))
+    // ok relativePath is stated to live under localRoot, but localRoot and remoteRoot need to exist
+    .chain(config =>
+      doRootPathsExist(config)
+        // work out the relative path we'd have on remote
+        .map(_ => calculateRemotePath(localPath, config))
+        .chain(remotePath =>
+          // is file in remote? If not, be specific about why we're exiting
+          checkFile(remotePath)
+            .orElse(fileError => rejected(`File Not In Remote Folder: ${fileError}`))
+            .chain(remoteStat =>
+              checkFile(localPath)
+                // the files in both places, check dest is smaller, or maybe they are same file
+                .chain(localStat =>
+                  getSize(remoteStat).chain(remoteSize =>
+                    getSize(localStat).chain(
+                      localSize =>
+                        equal(remoteSize, localSize) // filesize is equal, but check really same before deciding
+                          ? checkReallyEqual(remotePath, localPath)
+                          : copyIfLocalSmaller(localPath, localSize, remotePath, remoteSize)
                     )
                   )
-                  /* we need to put a sad path on the happy path (failed local stat), we're now
+                )
+                /* we need to put a sad path on the happy path (failed local stat), we're now
                    * responsible for making sure that's why we got here */
-                  .orElse(err => copyIfLocalNotFound(err, localPath, remotePath))
-              )
-          )
-      )
-  )
-}
+                .orElse(err => copyIfLocalNotFound(err, localPath, remotePath))
+            )
+        )
+    )
 
 module.exports = { synctool }
