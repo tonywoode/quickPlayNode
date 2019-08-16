b5c6f91780ada3845ac778961d1f12f6c6920559
tony woode <tonywoodecode@gmail.com>
2019-08-16 19:01:18 +0100

    [synctool] minor whitespace

--- src/synctool/index.js
+++ src/synctool/index.js
@@ -20,20 +20,21 @@ const synctool = (localPath, configFileName) =>
     // so we have a valid path and a root path, is path in root path
     .chain(config => isLocalPathInRootPath(config, localPath))
     // ok relativePath is stated to live under localRoot, but localRoot and remoteRoot need to exist
-    .chain(config =>
-      doRootPathsExist(config)
-        // work out the relative path we'd have on remote
-        .map(_ => calculateRemotePath(localPath, config))
-    )
+    .chain(config => doRootPathsExist(config).map(_ => calculateRemotePath(localPath, config)))
+    // worked out the relative path we'd have on remote
     .chain(remotePath =>
       checkRemoteFile(remotePath)
+        // remote exists
         .chain(remoteStat =>
           checkLocalFile(localPath)
             // the files in both places, check dest is smaller, or maybe they are same file
             .chain(localStat =>
               getSize(remoteStat)
-                .chain(remoteSize => getSize(localStat).map(localSize => ({ remoteSize, localSize })))
-                .chain( ({ remoteSize, localSize }) =>
+                .chain(remoteSize =>
+                  getSize(localStat).map(localSize => ({ remoteSize, localSize }))
+                )
+                .chain(
+                  ({ remoteSize, localSize }) =>
                     equal(remoteSize, localSize) // filesize is equal, but check really same before deciding
                       ? dontCopyIfEqual(remotePath, localPath)
                       : copyIfLocalSmaller(localPath, localSize, remotePath, remoteSize)
