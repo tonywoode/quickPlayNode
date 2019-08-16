2c7b09df40aa4c90915c1afe8f5c8093982a9cd5
tony woode <tonywoodecode@gmail.com>
2019-08-16 08:34:08 +0100

    [synctool] remove some nesting

--- src/synctool/index.js
+++ src/synctool/index.js
@@ -24,27 +24,27 @@ const synctool = (localPath, configFileName) =>
       doRootPathsExist(config)
         // work out the relative path we'd have on remote
         .map(_ => calculateRemotePath(localPath, config))
-        .chain(remotePath =>
-          // is file in remote? If not, be specific about why we're exiting
-          checkFile(remotePath)
-            .orElse(fileError => rejected(`File Not In Remote Folder: ${fileError}`))
-            .chain(remoteStat =>
-              checkFile(localPath)
-                // the files in both places, check dest is smaller, or maybe they are same file
-                .chain(localStat =>
-                  getSize(remoteStat).chain(remoteSize =>
-                    getSize(localStat).chain(
-                      localSize =>
-                        equal(remoteSize, localSize) // filesize is equal, but check really same before deciding
-                          ? checkReallyEqual(remotePath, localPath)
-                          : copyIfLocalSmaller(localPath, localSize, remotePath, remoteSize)
-                    )
-                  )
+    )
+    // is file in remote? If not, be specific about why we're exiting
+    .chain(remotePath =>
+      checkFile(remotePath)
+        .orElse(fileError => rejected(`File Not In Remote Folder: ${fileError}`))
+        .chain(remoteStat =>
+          checkFile(localPath)
+            // the files in both places, check dest is smaller, or maybe they are same file
+            .chain(localStat =>
+              getSize(remoteStat).chain(remoteSize =>
+                getSize(localStat).chain(
+                  localSize =>
+                    equal(remoteSize, localSize) // filesize is equal, but check really same before deciding
+                      ? checkReallyEqual(remotePath, localPath)
+                      : copyIfLocalSmaller(localPath, localSize, remotePath, remoteSize)
                 )
-                /* we need to put a sad path on the happy path (failed local stat), we're now
-                   * responsible for making sure that's why we got here */
-                .orElse(err => copyIfLocalNotFound(err, localPath, remotePath))
+              )
             )
+            /* we need to put a sad path on the happy path (failed local stat), we're now
+                   * responsible for making sure that's why we got here */
+            .orElse(err => copyIfLocalNotFound(err, localPath, remotePath))
         )
     )
 
