d571318315749ae5042e077f4112eac57fa8a6d4
tony woode <tonywoodecode@gmail.com>
2019-09-02 09:09:39 +0100

    [synctool] more ctrl+d issues...

--- src/helpers/ctrlCToQuit.js
+++ src/helpers/ctrlCToQuit.js
@@ -1,10 +1,10 @@
 // https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js/14861513#14861513
 // its said SIGINT should work on any platform now, i didn't find that to be true, the below finally makes windows cmd  quit,
 // but doesn't print the message, I suspect signals are working on powershell but not cmd
-// TODO: what code to exit with, and anyway is windows receiving the wxit code (havent seen the message print very often)?
-module.exports = (exitCode) => {
+// TODO: what code to exit with, and anyway is windows receiving the exit code (havent seen the message print very often)?
+// may cause issues with the process.exit see - https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+module.exports = exitCode => {
   const message = '[QPNode] exiting...)'
-  process.on('SIGINT', () => console.log(message) || process.exit(exitCode))
   if (process.platform === 'win32') {
     require('readline')
       .createInterface({
@@ -12,5 +12,7 @@ module.exports = (exitCode) => {
         output: process.stdout
       })
       .on('SIGINT', () => process.emit('SIGINT'))
+    // this causes *nix to not be able to exit on ctrl+c, hence moved inside here
+    process.on('SIGINT', () => console.log(message) || process.exit(exitCode))
   }
 }
