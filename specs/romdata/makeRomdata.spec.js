9ec1398974b238a5b3731212d7c1ba7a796b372f
tony woode <tonywoodecode@gmail.com>
2019-10-02 19:14:16 +0100

    [mametool] paths - fix tests

--- specs/romdata/makeRomdata.spec.js
+++ specs/romdata/makeRomdata.spec.js
@@ -34,12 +34,13 @@ const expected = [ `ROM DataFile Version : 1.1`,
 
 describe(`makeRomdata`, () => {
   it(`should accept a mame json string and return something`, () => {
-    const systems = makeRomdata(`Mame64 Win32`)(mockJson)
+    const systems = makeRomdata(`anything`)(mockJson)
     expect(systems).to.not.be.null
   })
 
   it(`should convert the input data to expected romdata lines, regardless of other ini filling of the romdata that also occurs`, () => {
-    const systems = makeRomdata(`Mame64 Win32`)(mockJson)
+    const settings = { mameExe : `Mame64 Win32`}
+    const systems = makeRomdata(settings)(mockJson)
     const systemsWithoutUndef = systems.map( element => element.replace(/undefined/g, ``) )
     expect(systemsWithoutUndef).to.deep.equal(expected)
   })
