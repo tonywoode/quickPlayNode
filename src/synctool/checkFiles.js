bc9eea40e6ccbc5573237ad7b7b58b52f049196f
tony woode <tonywoodecode@gmail.com>
2019-10-04 13:26:12 +0100

    [synctool] recursive symlink checker...
    ...not the most efficient imp, but now we can have a symlink at
    any point in the destination path and it will divert file/subdir writing
    etc correctly

--- src/synctool/checkFiles.js
+++ src/synctool/checkFiles.js
@@ -32,27 +32,65 @@ const isFile = stat => (isObject(stat) ? Just(stat.isFile()) : Nothing())
 // don't switch on a returned error, the local file may very well not exist, we're only
 // interested in changing the path if we find there's an existing symlink in the destination folder
 // TODO: recursive version of this using path.dirname that will find a symlink on any level
+// why not use fs.realpath? because the file needs to exist....
 // Path -> Task Error Path
 const getRealPath = filePath =>
   task(r => {
-    // first check the target itself is a symlink
-    fs.readlink(filePath, (err, target) => {
-      err
-        ? // if not, check the targets parent dir is a symlink, handy for mame chds
-        fs.readlink(path.dirname(filePath), (err, parentTarget) => {
-          err
-            ? r.resolve(filePath) // assume its not a symlink
-            : (console.log(
-              `[synctool] - destination's parent dir is a symlink real path set to ${path.join(
-                parentTarget,
-                path.basename(filePath)
-              )}`
-            ),
-            r.resolve(path.join(parentTarget, path.basename(filePath))))
-        })
-        : (console.log(`[synctool] - destination is a symlink real path set to ${target}`),
-        r.resolve(target))
-    })
+    const workingPath = filePath.split(/[\\/]/)
+    const returnPath = []
+    let outputPath = ''
+
+    //i'm not sure reduce is best for this, as you can't bail out, yet we need to when either we find a non-existing but or we find a symlink
+    const result = workingPath.reduce((returnPath, currentValue, index, array) => {
+      //if (filePath.includes(returnPath) === false) { return returnPath} //we already changed our path because of a symlink
+      console.log("checking " + path.join(returnPath, currentValue))
+      //const bitOfPath = workingPath.shift()
+      //returnPath.push(bitOfPath)
+ //     if (fs.existsSync(path.join(returnPath, currentValue)) === false) {
+ //         console.log(currentValue + " does not exist")
+ //       if (outputPath === '') {
+ //         outputPath = filePath // some part of this path isn’t real, we’re done, but since we can't exit a reduce early, do this
+ //         return returnPath
+ //       }
+ //     } else {
+        console.log("checking if " + path.join(returnPath, currentValue) + " is a symlink")
+        //is it a symlink
+        try {
+        const realPath = fs.readlinkSync( path.join(returnPath, currentValue))
+              console.log("I ONLY WENT AND FOUND A FUCKING SYMLINK " + currentValue)
+              //array = []
+              if (outputPath === '') return path.join(realPath) //outputPath = (path.join(realPath, workingPath.toString()))
+        } catch {
+          console.log(path.join(returnPath, currentValue) + "isnt a symlink")
+      return path.join(returnPath, currentValue)
+        }
+         })
+    if (outputPath === '') outputPath = filePath
+    console.log('real output path is ' + result)
+    //process.exit()
+    r.resolve(result)
   })
+
+// const getRealPath = filePath =>
+//  task(r => {
+//    // first check the target itself is a symlink
+//    fs.readlink(filePath, (err, target) => {
+//      err
+//        ? // if not, check the targets parent dir is a symlink, handy for mame chds
+//        fs.readlink(path.dirname(filePath), (err, parentTarget) => {
+//          err
+//            ? r.resolve(filePath) // assume its not a symlink
+//            : (console.log(
+//              `[synctool] - destination's parent dir is a symlink real path set to ${path.join(
+//                parentTarget,
+//                path.basename(filePath)
+//              )}`
+//            ),
+//            r.resolve(path.join(parentTarget, path.basename(filePath))))
+//        })
+//        : (console.log(`[synctool] - destination is a symlink real path set to ${target}`),
+//        r.resolve(target))
+//    })
+//  })
 
 module.exports = { stat, isDir, isFile, getRealPath }
