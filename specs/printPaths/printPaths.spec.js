100e62009757e7f600ebaf70d4cd0236d646f540
tony woode <tonywoodecode@gmail.com>
2022-03-01 19:25:57 +0000

    [mame] remove string similarity print paths code...
    because rompaths are arbitrary, we now use a UI to prompt the user to
    fill in rompath types, we don't try and automatically work out path
    types from the mame ini's mame rompath setting. So remove all the code
    that tried to do that, it was wrong...
    
    also remove tests, but add tests for getting rompaths. Can't really test
    determinePathToMameIni because its too impure

--- specs/printPaths/printPaths.spec.js
+++ specs/printPaths/printPaths.spec.js
@@ -2,321 +2,40 @@
 
 const path = require('path')
 const {
-  makeRomPathsAbs,
-  addMameFilePathsToSettings,
-  fillRomPaths,
-  checkForDupes,
-  sanitiseRomPaths,
-  rateADifferenceObject,
-  rateAllRomPaths,
-  rateUsersRompaths,
-  rateEachFolderForEachType,
-  getLowestDistanceForTypes
+  getMameIniRomPath,
+  makeRomPathsAbs
 } = require('../../src/printPaths/printPaths.js')
-const no = _ => false
-const yes = console.log
-global.log = global.log || {
-  filePaths: yes // to keep test output clean feel free to change it
-}
+
 
 describe('printPaths', () => {
-  const isItRetroArch = false // TODO: if true, things start to equal undefined?
 
-describe.only( 'makeRomPathsAbs', () => {
-  it('inserts absolute path from 2nd param if paths are relative', () => {
-    const mameEmuDir = 'C:/Emulators/Mame'
-    const filepath =  'ROMS;CHDs;Software List ROMS;Software List CHDs'
-    const absoluteFilepath = makeRomPathsAbs(filepath, mameEmuDir)
-    //we can't do this, because path.resolve works like this:
-    // "If, after processing all given path segments, an absolute path has not yet been generated, the current working directory is used."
-    // meaning on nix you'll get your path to this script appended to the front
-    //const expected =   "C:/Emulators/Mame/ROMS;C:/Emulators/Mame/CHDs;C:/Emulators/Mame/Software List ROMS;C:/Emulators/Mame/Software List CHDs"
-    //best we can do is check that we have the windows absolute path included in the output
-    expect(absoluteFilepath).to.include(mameEmuDir)
-  })
-})
-
-  describe('rompath splitting', () => {
-    // internally, we later will get the foldername of this exe, that's why we pass in a nonexistent exe
-    const mameEmuDir = './specs/printPaths/mameIniAbsolute'
-
-    it('looks up a real ini file from the filesystem', () => {
-      const devMode = false // regrettable, otherwise we look at test mame.ini in root
-      const paths = addMameFilePathsToSettings(mameEmuDir, isItRetroArch, devMode)
-      expect(paths.mameRoms).to.equal('F:\\MAME\\ROMS')
-      expect(paths.mameChds).to.equal('')
+  describe('getMameIniRomPath', () => {
+    const theRomPathInTheAbsolutIni = 'F:\\MAME\\ROMS'
+    const theRomPathInTheRelativeIni = 'ROMS;CHDs'
+    it('gets the contents of an absolute rompath string', () => {
+      const result = getMameIniRomPath('./specs/printPaths/mameIniAbsolute/mame.ini', 'foobar')
+      expect(result).to.equal(theRomPathInTheAbsolutIni)
     })
 
-    it('fills in just settings.mameRoms if mame.ini has only one rompath', () => {
-      const romPath = 'F:\\MAME\\ROMS'
-      const paths = fillRomPaths([romPath])
-      expect(paths.mameRoms).to.equal(romPath)
-      expect(paths.mameChds).to.equal('')
-    })
-    // now use the mame.ini in root, the one that has all 4 paths, quoted and absolute
-    it('fills in individual paths to different content types if more than one rompath in mame ini', () => {
-      const romPath1 = 'F:\\MAME\\ROMS'
-      const romPath2 = 'F:\\MAME\\CHDs'
-      const romPaths = [romPath1, romPath2]
-      const paths = fillRomPaths(romPaths)
-      expect(paths.mameRoms).to.equal(romPath1)
-      expect(paths.mameChds).to.equal(romPath2)
-    })
-    // it('detects if one rompath is of the expected type but others are not, and tries to use them as appropriate', () => {
-    //   expect(false).to.equal(true)
-    // })
-  })
-  // test for adding the absolute path if the mame.ini only contains a relative path
-  describe('absolute path', () => {
-    // this is a bit contrived, we're relying on this:
-    // path.win32.isAbsolute(romPathPart) to be false here for this test to pass, without the win32 it woule be false anyway, but the tests above would fail as they would see windows paths as not absolute
-    it('changes a single path to absolute if relative', () => {
-      const relativeExePath = `./specs/printPaths/mameIniRelative`
-      const absoluteExePath = path.resolve(relativeExePath)
-      const mameEmuDir = `${relativeExePath}`
-      let devMode = false
-      const paths = addMameFilePathsToSettings(mameEmuDir, isItRetroArch, devMode)
-      expect(paths.mameRoms).to.equal(path.join(absoluteExePath, 'ROMS'))
-      expect(paths.mameChds).to.equal(path.join(absoluteExePath, 'CHDs'))
-    })
-
-    it('does not alter a windows absolute path, despite the tests possibly running on nix', () => {
-      const relativeExePath = `./specs/printPaths/mameIniAbsolute`
-      const absoluteExePath = path.resolve(relativeExePath)
-      const expectedWinPath = 'F:\\MAME'
-      const mameEmuDir = `${absoluteExePath}`
-      let devMode = false
-      const paths = addMameFilePathsToSettings(mameEmuDir, isItRetroArch, devMode)
-      // win32 join because otherwise our path separator would be nix
-      expect(paths.mameRoms).to.equal(path.win32.join(expectedWinPath, 'ROMS'))
-      // ideally we'd have 4 mame.inis so we can check all combos of single path, all paths, and abs relative
+    it('gets the contents of a relative rompath string', () => {
+      const result = getMameIniRomPath('./specs/printPaths/mameIniRelative/mame.ini', 'foobar')
+      expect(result).to.include('foobar/ROMS') //see note following for why this is the best we can do
     })
   })
 
-  describe('sanitiseRomPath', () => {
-    it('when supplied an array of paths, removes the string "mame" from the basename', () => {
-      const pretendPaths = ['/mamechds', '/me/mine/romsmame']
-      const sanitised = sanitiseRomPaths(pretendPaths)
-      expect(sanitised).to.deep.equal([
-        {
-          basename: 'mamechds',
-          basenameNoMame: 'chds',
-          romPathAbs: '/mamechds'
-        },
-        {
-          basename: 'romsmame',
-          basenameNoMame: 'roms',
-          romPathAbs: '/me/mine/romsmame'
-        }
-      ])
+
+  describe('makeRomPathsAbs', () => {
+    it('inserts absolute path from 2nd param if paths are relative', () => {
+      const mameEmuDir = 'C:/Emulators/Mame'
+      const filepath = 'ROMS;CHDs;Software List ROMS;Software List CHDs'
+      const absoluteFilepath = makeRomPathsAbs(filepath, mameEmuDir)
+      //we can't do this, because path.resolve works like this:
+      // "If, after processing all given path segments, an absolute path has not yet been generated, the current working directory is used."
+      // meaning on nix you'll get your path to this script appended to the front
+      //const expected =   "C:/Emulators/Mame/ROMS;C:/Emulators/Mame/CHDs;C:/Emulators/Mame/Software List ROMS;C:/Emulators/Mame/Software List CHDs"
+      //best we can do is check that we have the windows absolute path included in the output
+      expect(absoluteFilepath).to.include(mameEmuDir)
     })
   })
 
-  describe('duplicateFoldernames', () => {
-    it('removes duplicate basename in our santitised list, and alerts us', () => {
-      // note arrays expected to always match arity/order
-      //  const romPathsAbs = ['/pathA/roms', '/pathB/roms', '/pathC/mamechds', '/pathD/chds']
-      //  const noMameString = ['roms', 'roms', 'chds', 'chds']
-
-      const romPathsObjArr = [
-        {
-          basenameNoMame: 'roms',
-          romPathAbs: '/pathA/roms'
-        },
-        {
-          basenameNoMame: 'roms',
-          romPathAbs: '/pathB/roms'
-        },
-        {
-          basenameNoMame: 'chds',
-          romPathAbs: '/pathC/mamechds'
-        },
-        {
-          basenameNoMame: 'chds',
-          romPathAbs: '/pathD/chds'
-        }
-      ]
-
-      expect(checkForDupes(romPathsObjArr)).to.deep.equal([
-        {
-          basenameNoMame: 'roms',
-          romPathAbs: '/pathA/roms'
-        },
-        {
-          basenameNoMame: 'chds',
-          romPathAbs: '/pathC/mamechds'
-        }
-      ])
-    })
-  })
-
-  describe('distance', () => {
-    const stoogesDiffObj = [
-      {
-        basenameNoMame: 'Mo'
-      },
-      {
-        basenameNoMame: 'Larry'
-      },
-      {
-        basenameNoMame: 'Curly'
-      }
-    ]
-
-    it('rates a difference object', () => {
-      const obj1 = stoogesDiffObj[0]
-      const romPathTypes = ['Roms', 'Chds', 'SoftwareListRoms', 'SoftwareListChds']
-      const rated = rateADifferenceObject(romPathTypes, obj1)
-      expect(rated).to.deep.equal({
-        basenameNoMame: 'Mo',
-        Roms: 0,
-        Chds: 0,
-        SoftwareListRoms: 0,
-        SoftwareListChds: 0
-      })
-    })
-
-    it(`rates an Object Array of difference objects`, () => {
-      const romPathTypes = ['Roms', 'Chds', 'SoftwareListRoms', 'SoftwareListChds']
-      const rated = rateAllRomPaths(romPathTypes)(stoogesDiffObj)
-      expect(rated).to.deep.equal([
-        {
-          basenameNoMame: 'Mo',
-          Roms: 0,
-          Chds: 0,
-          SoftwareListRoms: 0,
-          SoftwareListChds: 0
-        },
-        {
-          basenameNoMame: 'Larry',
-          Roms: 0,
-          Chds: 0,
-          SoftwareListRoms: 0.10526315789473684,
-          SoftwareListChds: 0.10526315789473684
-        },
-        {
-          basenameNoMame: 'Curly',
-          Roms: 0,
-          Chds: 0,
-          SoftwareListRoms: 0,
-          SoftwareListChds: 0
-        }
-      ])
-    })
-
-    it(`rates an Object Array of user difference objects`, () => {
-      // these should be absolute paths
-      const romPaths = ['mameRomFolders', 'mameChdFolder', 'mameSoftlists', 'mameSoftChds']
-      const romPathTypes = ['Roms', 'Chds', 'SoftwareListRoms', 'SoftwareListChds']
-      const result = rateUsersRompaths(romPathTypes, romPaths)
-      expect(result).to.deep.equal([
-        {
-          romPathAbs: 'mameRomFolders',
-          basename: 'mameRomFolders',
-          basenameNoMame: 'RomFolders',
-          Roms: 0.3333333333333333,
-          Chds: 0,
-          SoftwareListRoms: 0.16666666666666666,
-          SoftwareListChds: 0
-        },
-        {
-          romPathAbs: 'mameChdFolder',
-          basename: 'mameChdFolder',
-          basenameNoMame: 'ChdFolder',
-          Roms: 0,
-          Chds: 0.36363636363636365,
-          SoftwareListRoms: 0,
-          SoftwareListChds: 0.17391304347826086
-        },
-        {
-          romPathAbs: 'mameSoftlists',
-          basename: 'mameSoftlists',
-          basenameNoMame: 'Softlists',
-          Roms: 0,
-          Chds: 0,
-          SoftwareListRoms: 0.43478260869565216,
-          SoftwareListChds: 0.43478260869565216
-        },
-        {
-          romPathAbs: 'mameSoftChds',
-          basename: 'mameSoftChds',
-          basenameNoMame: 'SoftChds',
-          Roms: 0,
-          Chds: 0.6,
-          SoftwareListRoms: 0.2727272727272727,
-          SoftwareListChds: 0.6363636363636364
-        }
-      ])
-    })
-
-    it('when supplied a list of path types and a path, rates the path for the types', () => {
-      const romPathTypes = [
-        'C:/me/Roms',
-        'D:/ROMS/SO_ROMS/Chds',
-        'X:/Child/Chad/Ched/SoftwareListRoms',
-        'P:/SoftwareListChds'
-      ]
-      const romPath = 'roms'
-      const distances = rateEachFolderForEachType(romPath, romPathTypes)
-      expect(distances)
-        .to.be.an('array')
-        .of.length(4)
-    })
-  })
-
-  // we could do with snapshots now. Here's what distances i got with these inputs
-  // 'roms' [ 1, 3, 12, 14 ]
-  // 'chds' [ 3, 1, 15, 13 ]
-  // 'softlist_roms' [ 10, 11, 9, 10 ]
-  // 'softlist_chd' [ 10, 11, 10, 9 ]
-
-  it('when supplied ratings for each folder, picks the best for a romtype', () => {
-    const yourRomPathBasenames = ['roms', 'chds', 'softlist_roms', 'softlist_chd']
-    const romPathTypes = ['Roms', 'Chds', 'SoftwareListRoms', 'SoftwareListChds']
-    const allDistances = [[1, 3, 12, 14], [3, 1, 15, 13], [10, 11, 9, 10], [10, 11, 10, 9]]
-    const allDistancesObj = [
-      {
-        name: 'Roms',
-        roms: 1,
-        chds: 3,
-        softwareListRoms: 12,
-        softwareListChds: 14
-      },
-      {
-        name: 'Chds',
-        roms: 3,
-        chds: 1,
-        softwareListRoms: 15,
-        softwareListChds: 13
-      },
-      {
-        name: 'SoftwareListRoms',
-        roms: 10,
-        chds: 11,
-        softwareListRoms: 9,
-        softwareListChds: 10
-      },
-      {
-        name: 'mameSoftwareListChds',
-        roms: 10,
-        chds: 11,
-        softwareListRoms: 10,
-        softwareListChds: 9
-      }
-    ]
-
-    // so we get an array of objects with the name of each basename
-    const bases = yourRomPathBasenames.map(basename => ({ name: basename }))
-    // now we need to give each object a field for each type
-    //  const things = bases.map( base => ({ name: base.name, romPathTypes }))
-    const addArrAsObjKeys = (arr, obj) =>
-      arr.reduce((obj, key, idx) => ({ ...obj, [key]: '' }), obj)
-    const inserted = bases.map(base => addArrAsObjKeys(romPathTypes, base))
-    // now we use Object.keys to do our differences on....
-    console.log(inserted)
-    // process.exit()
-    const answer = getLowestDistanceForTypes(romPathTypes, allDistances)
-    // what answer do we want?
-    console.log(answer)
-  })
 })
--- src/printPaths/printPaths.js
+++ src/printPaths/printPaths.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const fs = require('fs')
-var stringSimilarity = require('string-similarity')
-const { map, pipe } = require('ramda')
 
 // work out where the mame ini file is on your filesystem
 const determinePathToMameIni = (mameEmuDir, isItRetroArch, mameIniFileName, messIniFileName) => {
@@ -32,111 +30,6 @@ const getMameIniRomPath = (mameIniPath, mameEmuDir) => {
   }
 }
 
-const makeRomPathsObjArr = romPathAbs => ({ romPathAbs})
-// basename is the final rightmost segment of file path; usually a file, but can also be directory
-const addBasename = ({romPathAbs}) => ({ romPathAbs, basename: path.win32.basename(romPathAbs) })
-const removeMameStringFromPath = romPathObj => ({...romPathObj, basenameNoMame: romPathObj.basename.replace(/mame/i, '') })
-
-
-// if >1 sanitised basename is the same, first wins and warn user to symlink if they want it fixed (should test absolute names in this event)
-// so we need to make a list of the basenames on each object and check they are unique, we can indedc use the index of the object made to get back to each object because we have an array of objects
-const checkForDupes = romPathsObjArr => {
-  // we can match up idxs on this
-  const basenamesNoMame = romPathsObjArr.map( romPathsObj => romPathsObj.basenameNoMame)
-  const removedIdxs = idxsOfDupes(basenamesNoMame)
-  removedIdxs.length !== 0 &&
-    console.log(
-      `Print Paths - Warning: Mame filepath printing may not work properly: found more than one Mame Rompath that seems to deal with the same content. We're going to use the first path found - removed: ${removedIdxs.map(
-        idx => romPathsObjArr[idx].romPathAbs
-      )} . You can make all rompaths work by symlinking the files from the non-chosen folders into the chosen ones`
-    )
-  return romPathsObjArr.filter( (el, idx) => !removedIdxs.includes(idx) )
-}
-
-// get your rompaths ready for difference comparison: turn into basenames, check for duplicates (warning if so), remove the string 'mame'
-// conumdrum: we don't want to remove 'mame' first incase we have 'mameroms' and 'romsmame', but if we don't we get two dupes 'roms', we've got little choice but to compare post-removal
-const sanitiseRomPaths = romPathsAbs =>
-  pipe(
-    map(makeRomPathsObjArr),
-    map(addBasename),
-    map(removeMameStringFromPath),
-    checkForDupes
-  )(romPathsAbs)
-
-/* DISTANCE FNS */
-const romPathTypes = ['Roms', 'Chds', 'SoftwareListRoms', 'SoftwareListChds']
-
-// now that each rompath is rated, if we have more than one for each type, we need to take the most likely, so returns 4 rompaths
-// takes your rompaths and rates each for closeness to mame's rompath types
-const rateRomPath = (romPath, romPathType) =>
-  stringSimilarity.compareTwoStrings(romPath, romPathType)
-
-// for each of mame's rompath types, returns the rompath (out of 4) that is the most likely container for that rompath type
-const rateEachFolderForEachType = (romPath, romPathTypes) =>
-  romPathTypes.map(pathType => rateRomPath(romPath, pathType))
-
-// difference object will become { name: 'foo', Roms: '3', Chds: '6', ... }
-const rateADifferenceObject = (romPathTypes, differenceObject) => ({
-  ...differenceObject,
-  ...Object.fromEntries(romPathTypes.map(key => [key, rateRomPath(differenceObject.basenameNoMame, key)]))
-})
-
-const rateAllRomPaths = romPathTypes => differenceObjects =>
-  differenceObjects.map(differenceObject => rateADifferenceObject(romPathTypes, differenceObject))
-
-const rateUsersRompaths = (romPathTypes, romPaths) =>
-  pipe(sanitiseRomPaths, rateAllRomPaths(romPathTypes))(romPaths)
-/// /////////////
-/// /////////////
-/// //////////////
-// trying not to do with arrays now
-const getLowestDistanceWithIdx = distances => {
-  const lowest = Math.min(...distances)
-  return [lowest, distances.indexOf(lowest)]
-}
-
-// make manual adjustments to the distance score of each rompath type
-
-const getLowestDistanceForTypes = (romPathTypes, allDistances) => {
-  return allDistances.reduce((acc, distances) => {
-    const distanceAndIdx = getLowestDistanceWithIdx(distances)
-    console.log([distanceAndIdx[0], romPathTypes[distanceAndIdx[1]]])
-    // we got an array of the distance and the index
-    // const lowestIdx = distances.indexOf(Math.min(...distances)) // this should pick the first if there's any same distances, which is concievable if one folder has mame in it and another doesnt
-    // console.log(lowestIdx)
-    // return lowestIdx
-  }, [])
-}
-
-/** basenames -> paths -> [] */
-const fillRomPaths = romPathsAbs => {
-  // first make a list of each of your rompaths, with slots for each of the rompathTypes for a rating of distance
-  // this just to get this test to pass atm, the imp needs replacing
-  const romPathsSanitised = sanitiseRomPaths(romPathsAbs).map( romPathObj => romPathObj.basenameNoMame)
-  console.log(romPathsSanitised)
-  const paths = {
-    mameRoms: '',
-    mameChds: '',
-    mameSoftwareListRoms: '',
-    mameSoftwareListChds: ''
-  }
-
-  if (romPathsAbs.length === 1) {
-    const theSingleRomPath = romPathsAbs[0]
-    log.filePaths(`only one path in your mame ini, make it all the params: ${theSingleRomPath}`)
-    paths.mameRoms = theSingleRomPath
-  } else {
-    // array order will be the same, test against basename but return absolute path
-    romPathsSanitised.forEach((rompath, idx) => {
-      ;/^.*Software List ROMS$/i.test(rompath) && (paths.mameSoftwareListRoms = romPathsAbs[idx])
-      ;/^.*Software List CHDs$/i.test(rompath) && (paths.mameSoftwareListChds = romPathsAbs[idx])
-      ;/^.*(?<!Software List )ROMS$/i.test(rompath) && (paths.mameRoms = romPathsAbs[idx])
-      ;/^.*(?<!Software List )CHDs$/i.test(rompath) && (paths.mameChds = romPathsAbs[idx])
-    })
-  }
-  return paths
-}
-
 // If you have relative paths in your mame rom path, then your mame roms must be on the same path as your mame emu
 // Not sure that QuickPlay is rooted such that it'll cd to your mame emu dir when running a romdata line,
 // So instead, makle the paths in that array absolute before we hand it to QuickPlay
@@ -153,50 +46,8 @@ const makeRomPathsAbs = (mameRomPath, mameEmuDir) => {
   const abs = romPaths.map(romPath => makeRomPathAbs(romPath, mameEmuDir))
   return abs.join(';')
 }
-
-/** determine the location of the mame.ini, this is only for printing filepaths, we just print a default if anything goes wrong...
- * path -> bool -> bool -> Object
- * @return an object with the 4 types of mame rom paths */
-const addMameFilePathsToSettings = (mameEmuDir, isItRetroArch, devMode) => {
-  const mameIniFileName = `./mame.ini`
-  const messIniFileName = `./mess.ini`
-  const mameIniPath = devMode
-    ? mameIniFileName
-    : determinePathToMameIni(mameEmuDir, isItRetroArch, mameIniFileName, messIniFileName)
-  const mameRomPath = getMameIniRomPath(mameIniPath, mameEmuDir)
-  const romPaths = mameRomPath.split(';')
-  // cater for the possibility that mame's rompath variable contains relative paths meaning they will be relative to mame's directory, any number of the paths may or may not be relative, make them all absolute since we're going to be following them from an unknown root
-  const romPathsAbs = romPaths.map(romPath => makeRomPathAbs(romPath, mameEmuDir))
-  log.filePaths(`MAME ini file:          found in ${mameIniPath}\nMAME ini Rompath:       ${romPaths}\n         Absolute:      ${romPathsAbs}`) // prettier-ignore
-  // and array is returned of the 4 romtypes
-  return fillRomPaths(romPathsAbs)
-}
-
-
-///////////////////////////////////////////////////////////////////////
-
-
-// utility fn returns the idxs of dupes in an array
-const idxsOfDupes = arr =>
-  arr
-    .map((element, idx) => {
-      if (arr.indexOf(element) !== idx) {
-        return idx
-      }
-    })
-    .filter(e => e != null)
-
 module.exports = {
   makeRomPathsAbs,
-  addMameFilePathsToSettings,
-  fillRomPaths,
-  checkForDupes,
   determinePathToMameIni,
   getMameIniRomPath,
-  sanitiseRomPaths,
-  rateADifferenceObject,
-  rateAllRomPaths,
-  rateUsersRompaths,
-  rateEachFolderForEachType,
-  getLowestDistanceForTypes
 }
