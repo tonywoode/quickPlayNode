f682e5edeb05afedc7a21fa0a3c8d96ec67076f6
tony woode <tonywoodecode@gmail.com>
2022-03-04 12:08:37 +0000

    [mame] add system-company fn for Amstrad ZX Spectrum...
    unfortunately formatting has obscured the actual fn I added:
    'replaceCompanyIfSystem' - the existing fns only changed either company
    or system but we needed to only change company IF a system name matched
    /ZX Spectrum/ - We've ended up with the emulator name still saying
    'Amstrad' but the system name and more importantly system type becoming
    Sinclair. This was in response to a change Mame made in 0.241

--- src/scan/datAndEfind/mungeCompanyAndSystemNames.js
+++ src/scan/datAndEfind/mungeCompanyAndSystemNames.js
@@ -10,35 +10,42 @@ const R = require('ramda')
 module.exports = systems => {
 
   const spaceIsSeparator = ` `
-  const oneWord          = 1
+  const oneWord = 1
 
   //make the basic new object structure that we want
   const systemsAugmented = R.pipe(
 
     //create+populate 2 new properties for munging TODO: why mutating?
-      R.map(obj => R.assoc(`mungedCompany`, obj.company, obj) )
-    , R.map(obj => R.assoc(`mungedSystem`,  obj.system,  obj) )
-  
+    R.map(obj => R.assoc(`mungedCompany`, obj.company, obj))
+    , R.map(obj => R.assoc(`mungedSystem`, obj.system, obj))
+
     //take company from system name if they repeat
     , R.map(obj => R.assoc(`mungedSystem`, obj.mungedSystem.replace(
-        new RegExp(`${obj.mungedCompany.split(spaceIsSeparator, oneWord)}\\W`, `i`), ``
-      ), obj 
-    )) 
+      new RegExp(`${obj.mungedCompany.split(spaceIsSeparator, oneWord)}\\W`, `i`), ``
+    ), obj
+    ))
 
   )(systems)
 
   //These are the main replacement functions to munge MESS' company name and system name. 
-  const compRep = (oldCompany, newCompany) => R.map( 
-    obj => R.assoc(`mungedCompany`, obj.mungedCompany.replace(oldCompany, newCompany), obj) 
+  const compRep = (oldCompany, newCompany) => R.map(
+    obj => R.assoc(`mungedCompany`, obj.mungedCompany.replace(oldCompany, newCompany), obj)
   )
 
-  //we match the company too when replacing the systeem name
-  const sysMatchRep = (thisCompany, oldsystem, newsystem) => R.map( 
-    obj => R.assoc(`mungedSystem`, ( obj.mungedCompany.match(thisCompany) && obj.mungedSystem.match(oldsystem) )? 
+  //we match the company too when replacing the system name
+  const sysMatchRep = (thisCompany, oldsystem, newsystem) => R.map(
+    obj => R.assoc(`mungedSystem`, (obj.mungedCompany.match(thisCompany) && obj.mungedSystem.match(oldsystem)) ?
       newsystem : obj.mungedSystem
-    , obj)
+      , obj)
   )
 
+  /* later, we had a problem where mame things its 'Amstrad ZX Spectrum' for some spectrums, yeah maybe, but isnt't it 
+   * FAR more important to keep all the spectrum's together under a type? Have a look at some pictures of "Amstrad"
+   * spectrums and of course they have Sinclair printed on them - remember we're oddly mutating these premade 'munged
+   * vars in the object*/
+  const replaceCompanyIfSystem = (oldCompany, newCompany, sysRegex = /ZX Spectrum/) => R.map(
+    obj => R.assoc(`mungedCompany`, sysRegex.test(obj.mungedSystem) ? obj.mungedCompany.replace(oldCompany, newCompany) : obj.mungedCompany, obj)
+  )
   /* transforms -  conventions and TODO's:
    *    - dont replace system name to ``, or system type will be `` (following code uses system name if mungedCompany|mungedSystem ='')
    *    - company name changes come after system changes
@@ -47,8 +54,8 @@ module.exports = systems => {
 
   const mungedSystems = R.pipe(
 
-      compRep(/(<unknown>|<generic>)/, ``)
-      //system specific (btw replace accepts regex or string by default (i'm trying to show what's intended), but match matches only regex
+    compRep(/(<unknown>|<generic>)/, ``)
+    //system specific (btw replace accepts regex or string by default (i'm trying to show what's intended), but match matches only regex
     , compRep(/Acorn .*/, `Acorn`), sysMatchRep(`Acorn`, /BBC|Electron/, `BBC`)
     , compRep(/Amstrad.*/, `Amstrad`), sysMatchRep(`Amstrad`, /(CPC|GX4000)/, `CPC`), sysMatchRep(`Amstrad`, /^PC([0-9]*).*/, `PC`)
     , compRep(`APF Electronics Inc.`, `APF`), sysMatchRep(`APF`, `M-1000`, `Imagination Machine`)
@@ -57,15 +64,15 @@ module.exports = systems => {
     , sysMatchRep(`Apple`, /\/\/\//, `III`)
     , sysMatchRep(`Atari`, /(400|^800.*|XE Game System)/, `400/600/800/1200/XE`) //don't match atari 7800
     , compRep(`Bally Manufacturing`, `Bally`)
-    , sysMatchRep(`Bandai`, `Super Vision 8000`, `Super Vision`) 
+    , sysMatchRep(`Bandai`, `Super Vision 8000`, `Super Vision`)
     , sysMatchRep(`Bondwell Holding`, /.*/, `Bondwell`), compRep(`Bondwell Holding`, ``) //change company after
     , sysMatchRep(`Camputers`, /Lynx/, `Lynx`)
     , sysMatchRep(`Casio`, `PV-1000`, `PV`)
     , sysMatchRep(`Central Data`, `CD 2650`, `2650`)
-    , compRep(`Commodore Business Machines`, `Commodore`), sysMatchRep(`Commodore`, /(B500|P500|B128-80HP)/, `500/600/700`) 
-      , sysMatchRep(`Commodore`, /PET .*|CBM .*/, `PET/CBM`), sysMatchRep(`Commodore`, /\b(64|128)/, `64/128`)
-      , sysMatchRep(`Commodore`, `VIC-10 / Max Machine / UltiMax`, `Max/Ultimax`), sysMatchRep(`Commodore`, `VIC-1001`, `VIC-20`)
-      , sysMatchRep(`Commodore`, `264`, `+4/C16`) 
+    , compRep(`Commodore Business Machines`, `Commodore`), sysMatchRep(`Commodore`, /(B500|P500|B128-80HP)/, `500/600/700`)
+    , sysMatchRep(`Commodore`, /PET .*|CBM .*/, `PET/CBM`), sysMatchRep(`Commodore`, /\b(64|128)/, `64/128`)
+    , sysMatchRep(`Commodore`, `VIC-10 / Max Machine / UltiMax`, `Max/Ultimax`), sysMatchRep(`Commodore`, `VIC-1001`, `VIC-20`)
+    , sysMatchRep(`Commodore`, `264`, `+4/C16`)
     , compRep(`Comx World Operations Ltd`, `COMX`)
     , compRep(`Cybiko Inc`, `Cybiko`)
     , sysMatchRep(`DEC`, /Rainbow/, `Rainbow`)
@@ -73,7 +80,7 @@ module.exports = systems => {
     , compRep(`Digital Equipment Corporation`, `DEC`)
     , sysMatchRep(`Dragon Data Ltd`, /128|Beta Prototype/, `128`), compRep(/Dragon Data Ltd|Dragon Compusense/, `Dragon`)
     , sysMatchRep(`EACA`, `Colour Genie EG2000`, `Colour Genie`)
-    , sysMatchRep(`Ei Nis`, `Pecom 32`, `Pecom`) 
+    , sysMatchRep(`Ei Nis`, `Pecom 32`, `Pecom`)
     , sysMatchRep(`Elektronika`, `BK 0010`, `BK`)
     , sysMatchRep(`Emerson`, `Arcadia 2001`, `Arcadia`)
     , sysMatchRep(`Enterprise Computers`, /Sixty Four|One Two Eight/, `64`), compRep(`Enterprise Computers`, `Enterprise`)
@@ -87,19 +94,19 @@ module.exports = systems => {
     , compRep(/.* Galaksija/, `Galaksija`)
     , compRep(`General Consumer Electronics`, `GCE`)
     , sysMatchRep(`Hewlett Packard`, /HP48*|/, `HP`), sysMatchRep(`Hewlett-Packard`, /9845./, `HP`)
-      , compRep(`Hewlett Packard`, `Hewlett-Packard`) //company last
-    , sysMatchRep(`Hawthorne Technology`, `TinyGiant HT68k`, `TinyGiant`) 
+    , compRep(`Hewlett Packard`, `Hewlett-Packard`) //company last
+    , sysMatchRep(`Hawthorne Technology`, `TinyGiant HT68k`, `TinyGiant`)
     , sysMatchRep(`Intel`, /iSBC 286/, `iSBC`)
     , compRep(`Interton Electronic`, `Interton`)
     , sysMatchRep(`Intelligent Game`, `Game MPT-03`, `Game`), compRep(`Intelligent Game`, `Intelligent`) //company last
     , compRep(`Jupiter Cantab`, `Jupiter`)
-      , sysMatchRep(`Kyosei`, `Kyotronic 85`, `Kyotronic`)
+    , sysMatchRep(`Kyosei`, `Kyotronic 85`, `Kyotronic`)
     , sysMatchRep(`Joseph Glagla and Dieter Feiler`, /Ravensburger Selbstbaucomputer.*/, `Ravensburger Selbstbaucomputer`)
-      , compRep(`Joseph Glagla and Dieter Feiler`, ``) //company last
+    , compRep(`Joseph Glagla and Dieter Feiler`, ``) //company last
     , compRep(`Kontiki Data A/S`, `Kontiki`) //company last
     , compRep(`Luxor Datorer AB`, `Luxor`), sysMatchRep(`Luxor`, /ABC.*/, `ABC`)
     , sysMatchRep(`Matra & Hachette`, `Alice 32`, `MC-10`), compRep(`Matra & Hachette`, `Tandy Radio Shack`) //company last
-    , compRep(`Memotech Ltd`, `Memotech`), sysMatchRep(`Memotech`, `MTX .*`, `MTX`) 
+    , compRep(`Memotech Ltd`, `Memotech`), sysMatchRep(`Memotech`, `MTX .*`, `MTX`)
     , sysMatchRep(`Mikroelektronika`, `Pyldin-601`, `Pyldin`)
     , sysMatchRep(`Micro Genius`, /IQ-.*/, `IQ`)
     , sysMatchRep(`Microkey`, `Primo A-32`, `Primo`)
@@ -108,11 +115,11 @@ module.exports = systems => {
     , sysMatchRep(`Micronique`, `Hector 2HR+`, `Hector`)
     , sysMatchRep(`Nascom Microcomputers`, `1|2`, `Nascom`), compRep(`Nascom Microcomputers`, ``) //company last
     , sysMatchRep(`Nintendo`, `Entertainment System / Famicom`, `NES`), sysMatchRep(`Nintendo`, `Game Boy Color`, `Game Boy`)
-      , sysMatchRep(`Nintendo`, `Super Entertainment System / Super Famicom `, `SNES`)
-    , compRep(`NEC / Hudson Soft`, `NEC`), sysMatchRep(`NEC`, `PC Engine`, `PC Engine/TurboGrafx-16`) 
-      , sysMatchRep(`NEC`, `PC-8201A`, `PC Series`)
+    , sysMatchRep(`Nintendo`, `Super Entertainment System / Super Famicom `, `SNES`)
+    , compRep(`NEC / Hudson Soft`, `NEC`), sysMatchRep(`NEC`, `PC Engine`, `PC Engine/TurboGrafx-16`)
+    , sysMatchRep(`NEC`, `PC-8201A`, `PC Series`)
     , sysMatchRep(`Non Linear Systems`, `Kaypro II - 2/83`, `Kaypro`)
-    , sysMatchRep(`NPO Microprocessor`, /Elektronika MS/,  `Elektronika`) 
+    , sysMatchRep(`NPO Microprocessor`, /Elektronika MS/, `Elektronika`)
     , compRep(`Nuova Elettronica`, `Nuova`)
     , compRep(`Orbit Electronics`, `Orbit`)
     , compRep(`Ormatu Electronics`, `Ormatu`)
@@ -134,9 +141,9 @@ module.exports = systems => {
     , sysMatchRep(`Samsung`, /SPC-1[05]00/, `SPC`)
     , sysMatchRep(`Sanyo`, /MBC-/, `MBC`), sysMatchRep(`Sanyo`, `PHC-25`, `PHC`)
     , sysMatchRep(`SNK`, /(Neo-Geo$|Neo-Geo AES)/, `Neo Geo`), sysMatchRep(`SNK`, `Neo-Geo CDZ`, `Neo Geo CD`) //wikipedia says MESS is wrong
-      , sysMatchRep(`SNK`, `NeoGeo Pocket`, `Neo Geo Pocket`) //MESS says MESS is wrong....
+    , sysMatchRep(`SNK`, `NeoGeo Pocket`, `Neo Geo Pocket`) //MESS says MESS is wrong....
     , sysMatchRep(`Sega`, `Genesis`, `Genesis/32X`), sysMatchRep(`Sega`, `Master System II`, `Master System`)
-       , sysMatchRep(`Sega`, /(SC-3000|SG-1000)/, `SG-1000/SC-3000/SF-7000`)
+    , sysMatchRep(`Sega`, /(SC-3000|SG-1000)/, `SG-1000/SC-3000/SF-7000`)
     , sysMatchRep(`Sharp`, /MZ.*/, `MZ`)
     , compRep(`Sinclair Research Ltd`, `Sinclair`), sysMatchRep(`Sinclair`, /ZX Spectrum .*/, `ZX Spectrum`)
     , sysMatchRep(`SME Systems`, `Aussie Byte II`, `Aussie Byte`)
@@ -158,6 +165,7 @@ module.exports = systems => {
     , sysMatchRep(`Video Technology`, /Laser.*/, `Laser Mk1`)
     , compRep(`Visual Technology Inc`, `Visual`)
     , sysMatchRep(`Watara`, `Super Vision`, `Supervision`) //again MESS seems to be wrong
+    , replaceCompanyIfSystem(/Amstrad.*/, 'Sinclair', /ZX Spectrum/) //now this corner case
 
   )(systemsAugmented)
 
